7206fb430376c733ccaf91f5332ef48be0ee25a3
niodtn <ipegte93@gmail.com>
2025-02-15 21:50:59 +0900

    Enhance Hammerspoon Hangul input switching functionality

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,24 +1,47 @@
 local excludeApps = {
-    "Moonlight"
+    "Moonlight"  -- Exclude Moonlight app
 }
+
+-- Switch between Hangul and English
+function toggleHangulInput()
+    local inputSources = hs.keycodes.inputSources()
+
+    -- Switch to Hangul
+    for _, inputSource in ipairs(inputSources) do
+        if inputSource.id == 'com.apple.keylayout.Hangul' then
+            hs.keycodes.setLayout(inputSource.id)
+            return
+        end
+    end
+
+    -- Switch to English
+    for _, inputSource in ipairs(inputSources) do
+        if inputSource.id == 'com.apple.keylayout.US' then
+            hs.keycodes.setLayout(inputSource.id)
+            return
+        end
+    end
+end
 
 Hangul = hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, function(event)
     local flags = event:getFlags()
-    local keyCode = event:getKeyCode()
-    local frontApp = hs.application.frontmostApplication():name()
+    local keyCode = event:getKeyCode()  -- Key pressed
+    local frontApp = hs.application.frontmostApplication():name()  -- Active app
 
+    -- Exclude specific apps
     for _, app in ipairs(excludeApps) do
         if frontApp == app then
             return false
         end
     end
 
+    -- Right Command key pressed
     if keyCode == hs.keycodes.map["rightcmd"] then
         if flags.cmd then
-            hs.eventtap.keyStroke({}, "f19")
+            toggleHangulInput()  -- Switch Hangul
         end
         return true
     end
 end)
 
-Hangul:start()
+Hangul:start()  -- Start listener
