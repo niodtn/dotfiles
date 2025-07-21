28f0dc2558b4bac855e66207a206860754dd655e
niodtn <ipegte93@gmail.com>
2025-07-21 23:17:49 +0900

    refactor: streamline flake.nix structure and improve module organization

--- nix/flake.nix
+++ nix/flake.nix
@@ -8,34 +8,43 @@
 
     mac-app-util.url = "github:hraban/mac-app-util";
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs =
-    inputs@{
-      self,
-      nix-darwin,
-      nixpkgs,
-      mac-app-util,
-      nix-homebrew,
-      ...
-    }:
-    let
-      global-configuration = [
-        {
-          # https://github.com/LnL7/nix-darwin/blob/master/modules/examples/flake/flake.nix
-          system.configurationRevision = self.rev or self.dirtyRev or null;
-          system.stateVersion = 6;
-          nix.settings.experimental-features = "nix-command flakes";
-        }
+    inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit self inputs; } {
+      systems = [
+        "aarch64-darwin"
+        "x86_64-linux"
       ];
-    in
-    {
-      darwinConfigurations = {
-        niodtn = nix-darwin.lib.darwinSystem {
-          system = "aarch64-darwin";
-          modules = global-configuration ++ [
-            mac-app-util.darwinModules.default
-            nix-homebrew.darwinModules.nix-homebrew
+
+      flake = {
+        darwinModules.default =
+          {
+            self,
+            config,
+            lib,
+            pkgs,
+            system,
+            ...
+          }:
+          {
+            # https://github.com/LnL7/nix-darwin/blob/master/modules/examples/flake/flake.nix
+            system.configurationRevision = self.rev or self.dirtyRev or null;
+            system.stateVersion = 6;
+            nix.settings.experimental-features = "nix-command flakes";
+          };
+
+        darwinConfigurations.mac = inputs.nix-darwin.lib.darwinSystem {
+          system = builtins.currentSystem;
+          specialArgs = { inherit inputs self; };
+
+          modules = [
+            self.darwinModules.default
+            inputs.mac-app-util.darwinModules.default
+            inputs.nix-homebrew.darwinModules.nix-homebrew
             ./mac/mac.nix
           ];
         };
