3fb14401b6ef8301795d2ff977d5a1715b6f348b
niodtn <ipegte93@gmail.com>
2025-07-27 12:04:25 +0900

    refactor: simplify darwinConfigurations structure in flake.nix

--- nix/flake.nix
+++ nix/flake.nix
@@ -21,34 +21,22 @@
         "x86_64-linux"
       ];
 
-      flake = {
-        darwinModules.default =
-          {
-            self,
-            config,
-            lib,
-            pkgs,
-            system,
-            ...
-          }:
+      # Darwin
+      flake.darwinConfigurations.mac = inputs.nix-darwin.lib.darwinSystem {
+        system = builtins.currentSystem;
+        specialArgs = { inherit inputs self; };
+
+        modules = [
+          inputs.mac-app-util.darwinModules.default
+          inputs.nix-homebrew.darwinModules.nix-homebrew
+          ./mac/mac.nix
           {
             # https://github.com/LnL7/nix-darwin/blob/master/modules/examples/flake/flake.nix
             system.configurationRevision = self.rev or self.dirtyRev or null;
             system.stateVersion = 6;
             nix.settings.experimental-features = "nix-command flakes";
-          };
-
-        darwinConfigurations.mac = inputs.nix-darwin.lib.darwinSystem {
-          system = builtins.currentSystem;
-          specialArgs = { inherit inputs self; };
-
-          modules = [
-            self.darwinModules.default
-            inputs.mac-app-util.darwinModules.default
-            inputs.nix-homebrew.darwinModules.nix-homebrew
-            ./mac/mac.nix
-          ];
-        };
+          }
+        ];
       };
     };
 }
