609ae78445eccdc50a43fb2b1713361f96abd41c
niodtn <ipegte93@gmail.com>
2025-08-03 23:21:56 +0900

    refactor: update Git configuration for user settings

--- nix/common/git.nix
+++ nix/common/git.nix
@@ -1,9 +1,11 @@
 { config, pkgs, ... }:
 
 {
-    home-manager.users.niodtn = {
-        "git.autofetch" = true;
-        "git.confirmSync" = false;
-        "git.suggestSmartCommit" = false;
-    }
+  home-manager.users.niodtn = {
+    programs.git = {
+      enable = true;
+      userName = "niodtn";
+      userEmail = "ipete93@gmail.com";
+    };
+  };
 }
