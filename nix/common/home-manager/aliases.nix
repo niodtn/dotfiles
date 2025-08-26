48f5113a82105ffad2caece17b0b36188b84282b
niodtn <ipegte93@gmail.com>
2025-08-26 15:25:23 +0900

    refactor: format aliases.nix for improved readability

--- nix/common/home-manager/aliases.nix
+++ nix/common/home-manager/aliases.nix
@@ -1,11 +1,11 @@
 { config, pkgs, ... }:
 
 {
-    # Shell aliases
-    home.shellAliases = {
-      ".." = "cd ../";
-      "..." = "cd ../../";
-      c = "clear";
-      gbc = "git branch | grep -v main | xargs git branch -D && git fetch -p";
-    };
+  # Shell aliases
+  home.shellAliases = {
+    ".." = "cd ../";
+    "..." = "cd ../../";
+    c = "clear";
+    gbc = "git branch | grep -v main | xargs git branch -D && git fetch -p";
+  };
 }
