bef2b04fdfefc107e284c9c79e6ebe0e1c599c41
niodtn <ipegte93@gmail.com>
2025-09-12 00:41:54 +0900

    feat: Add Git aliases for 'gpod' and 'gms'

--- nix/common/home-manager/aliases.nix
+++ nix/common/home-manager/aliases.nix
@@ -7,5 +7,7 @@
     "..." = "cd ../../";
     c = "clear";
     gbc = "git branch | grep -v main | xargs git branch -D && git fetch -p";
+    gpod = "git push origin --delete";
+    gms = "git merge --squash";
   };
 }
