8e66af2cc38aa1f7858a080f48f2c528e7b469be
niodtn <ipegte93@gmail.com>
2025-10-23 00:28:29 +0900

    feat: add Reaper DAW module

---
+++ nix/modules/nixos/reaper/default.nix
@@ -0,0 +1,17 @@
+{ lib, config, pkgs, ... }:
+with lib;
+
+let
+  cfg = config.modules.reaper;
+in
+{
+  options.modules.reaper = { enable = mkEnableOption "reaper"; };
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      reaper
+      reaper-sws-extension
+      reaper-reapack-extension
+    ];
+    security.rtkit.enable = true;
+  };
+}
