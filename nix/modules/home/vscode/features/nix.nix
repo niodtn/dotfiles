ab5e90775af708f62ea63d769a24c6c23efa4dce
niodtn <ipegte93@gmail.com>
2026-01-09 07:20:14 +0900

    feat: change formatter

--- nix/modules/home/vscode/features/nix.nix
+++ nix/modules/home/vscode/features/nix.nix
@@ -4,29 +4,30 @@
   pkgs,
   ...
 }:
-with lib;
-
-let
+with lib; let
   cfg = config.features.vscode.nix;
-
-in
-{
+in {
   options.features.vscode.nix = {
     enable = mkEnableOption "vscode nix feature";
   };
 
   config = mkIf cfg.enable {
-    home.packages = [ pkgs.nixfmt-rfc-style ];
+    home.packages = [pkgs.alejandra];
 
     programs.vscode.profiles.default = {
       extensions = with pkgs.vscode-extensions; [
         jnoortheen.nix-ide
+        kamadorueda.alejandra
       ];
 
       userSettings = {
         "[nix]" = {
-          "editor.defaultFormatter" = "jnoortheen.nix-ide";
+          "editor.defaultFormatter" = "kamadorueda.alejandra";
+          "editor.formatOnPaste" = true;
+          "editor.formatOnSave" = true;
+          "editor.formatOnType" = false;
         };
+        "alejandra.program" = "alejandra";
       };
     };
   };
