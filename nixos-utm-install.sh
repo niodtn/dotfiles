da744306a5a253948a2a7d9c4d38d3e93533c4a0
niodtn <ipegte93@gmail.com>
2026-01-24 00:49:21 +0900

    feat: add nixos-utm-install.sh
    chore: delete unnecessary file

---
+++ nixos-utm-install.sh
@@ -0,0 +1,19 @@
+#!bin/sh
+
+sudo parted /dev/vda -- mkpart ESP fat32 1MiB 512MiB
+sudo parted /dev/vda -- set 1 esp on
+sudo parted /dev/vda -- mkpart primary ext4 512MiB 100%
+
+sudo mkfs.fat -F 32 -n boot /dev/vda1
+sudo mkfs.ext4 -L nixos /dev/vda2
+
+sudo mount /dev/disk/by-label/nixos /mnt
+sudo mkdir -p /mnt/boot
+sudo mount /dev/disk/by-label/boot /mnt/boot
+
+sudo mkdir -p /mnt/tmp
+cd /mnt/tmp
+
+sudo nix-shell -p git --run "git clone https://github.com/niodtn/dotfiles"
+cd dotfiles
+sudo nixos-install --root /mnt --flake .#utm
