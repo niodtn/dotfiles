6c11d7572a1f670093e36b9f3b3b129e7f95301f
niodtn <ipegte93@gmail.com>
2026-01-24 03:27:38 +0900

    refactor: hardware.nix

--- nix/hosts/utm/configuration.nix
+++ nix/hosts/utm/configuration.nix
@@ -18,17 +18,8 @@
   services.spice-vdagentd.enable = true;
   services.qemuGuest.enable = true;
 
-  boot.initrd.availableKernelModules = [
-    "virtio_pci"
-    "virtio_blk"
-    "virtio_input"
-    "virtio_gpu"
-  ];
-
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  nixpkgs.hostPlatform = "aarch64-linux";
 
   username = "niodtn";
   users.users.${config.username} = {
