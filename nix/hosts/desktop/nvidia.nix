f483eb23e046f386a0f6400c29b538820d063bb6
niodtn <ipegte93@gmail.com>
2026-01-30 20:41:01 +0900

    update nvidia.nix

--- nix/hosts/desktop/nvidia.nix
+++ nix/hosts/desktop/nvidia.nix
@@ -1,18 +1,20 @@
 {config, ...}: let
-  nvidiaPkgs = config.boot.kernelPackages.nvidiaPackages.latest;
+  nvidiaPkgs = config.boot.kernelPackages.nvidiaPackages.stable;
 in {
   boot = {
     extraModulePackages = [nvidiaPkgs];
     blacklistedKernelModules = ["nouveau"]; # block open source driver
     kernelParams = ["nvidia-drm.modeset=1" "nvidia-drm.fbdev=1"];
+    initrd.kernelModules = ["nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm"];
   };
 
   hardware = {
     nvidia = {
       package = nvidiaPkgs;
       open = false; # closed source drivers
-      powerManagement.enable = true;
+      powerManagement.enable = false;
       modesetting.enable = true; # boot with nvidia modeset
+      # nvidiaSettings = true;
     };
 
     graphics.enable = true;
@@ -20,9 +22,9 @@ in {
   };
 
   environment.sessionVariables = {
-    GBM_BACKEND = "nvidia-drm";
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
     LIBVA_DRIVER_NAME = "nvidia";
+    NVD_BACKEND = "direct";
   };
 
   services.xserver.videoDrivers = ["nvidia"];
