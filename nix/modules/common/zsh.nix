2037e91528ae2ac1da439ef1a08a345161c18fd4
niodtn <71699228+niodtn@users.noreply.github.com>
2026-02-28 12:37:45 +0000

    feat: update zsh module

--- nix/modules/common/zsh.nix
+++ nix/modules/common/zsh.nix
@@ -11,18 +11,26 @@ in {
   options.features.zsh = {
     enable = mkEnableOption "zsh feature";
   };
-  config = mkIf cfg.enable {
-    programs.zsh.enable = true;
-    users.users.${username}.shell = pkgs.zsh;
 
-    # Home Manager Options
-    home-manager.users.${username} = {
-      programs.zsh = {
-        enable = true;
-        enableCompletion = true;
-        autosuggestion.enable = true;
-        syntaxHighlighting.enable = true;
+  config = mkIf cfg.enable (mkMerge [
+    # common
+    {
+      programs.zsh.enable = true;
+      users.users.${username}.shell = pkgs.zsh;
+
+      # Home Manager Options
+      home-manager.users.${username} = {
+        programs.zsh = {
+          enable = true;
+          enableCompletion = true;
+          autosuggestion.enable = true;
+          syntaxHighlighting.enable = true;
+        };
       };
-    };
-  };
+    }
+    # darwin
+    (optionalAttrs (options ? homebrew) {
+      homebrew.enableZshIntegration = true;
+    })
+  ]);
 }
