376a49df86a356ad5bf3df5456f5baf657a060df
niodtn <ipegte93@gmail.com>
2026-03-11 16:12:03 +0900

    feat: update atuin module

--- nix/modules/common/atuin.nix
+++ nix/modules/common/atuin.nix
@@ -8,21 +8,32 @@ with lib; let
 in {
   options.features.atuin = {
     enable = mkEnableOption "atuin feature";
+    sync.enable = mkEnableOption "atuin sync feature";
   };
 
-  config = mkIf cfg.enable {
-    home-manager.users.${config.username} = {
-      programs.atuin = {
-        enable = true;
-        daemon.enable = true;
-        settings = {
-          auto_sync = true;
-          sync_frequency = "0";
-          sync_address = "https://api.atuin.sh";
-          style = "auto";
-          invert = true;
+  config = mkIf cfg.enable (mkMerge [
+    {
+      home-manager.users.${config.username} = {
+        programs.atuin = {
+          enable = true;
+          settings = {
+            style = "auto";
+            invert = true;
+          };
         };
       };
-    };
-  };
+    }
+    (mkIf cfg.sync.enable {
+      home-manager.users.${config.username} = {
+        programs.atuin = {
+          daemon.enable = true;
+          settings = {
+            auto_sync = true;
+            sync_frequency = "0";
+            sync_address = "https://api.atuin.sh";
+          };
+        };
+      };
+    })
+  ]);
 }
