8d29609210dfeab4da2129a83a595a15634d957b
niodtn <ipegte93@gmail.com>
2026-04-24 01:08:03 +0900

    update hammerspoon module

--- nix/modules/darwin/hammerspoon.nix
+++ nix/modules/darwin/hammerspoon.nix
@@ -16,8 +16,11 @@ in {
     homebrew.casks = ["hammerspoon"];
 
     # Home Manager Options
-    home-manager.users.${username}.home.file.".hammerspoon" = {
-      source = "${flake}/hammerspoon";
+    home-manager.users.${username} = {lib, ...}: {
+      home.activation.linkHammerspoon = lib.hm.dag.entryAfter ["writeBoundary"] ''
+        $DRY_RUN_CMD rm -rf $HOME/.hammerspoon
+        $DRY_RUN_CMD ln -sfn $HOME/dotfiles/hammerspoon $HOME/.hammerspoon
+      '';
     };
   };
 }
