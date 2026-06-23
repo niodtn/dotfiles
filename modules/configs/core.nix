3c57917dd25a2f3255c6141a195de427253ec126
niodtn <ipegte93@gmail.com>
2026-06-24 01:41:36 +0900

    feat(core): add gc

--- modules/configs/core.nix
+++ modules/configs/core.nix
@@ -1,9 +1,17 @@
 {config, ...}: let
   common = {
-    nix.settings = {
-      experimental-features = ["nix-command" "flakes"];
-      extra-substituters = config.flake-file.nixConfig.extra-substituters;
-      extra-trusted-public-keys = config.flake-file.nixConfig.extra-trusted-public-keys;
+    nix = {
+      gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 14d --max-freed 10G";
+      };
+      settings = {
+        auto-optimise-store = true;
+        experimental-features = ["nix-command" "flakes"];
+        extra-substituters = config.flake-file.nixConfig.extra-substituters;
+        extra-trusted-public-keys = config.flake-file.nixConfig.extra-trusted-public-keys;
+      };
     };
     nixpkgs.config.allowUnfree = true;
   };
