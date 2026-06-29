1c1b0ae283a8b9e42abc222b1842f3c5dfe42731
niodtn <ipegte93@gmail.com>
2026-06-30 00:12:59 +0900

    feat(desktop): update hardware

--- hosts/desktop/hardware/default.nix
+++ hosts/desktop/hardware/default.nix
@@ -8,4 +8,16 @@
   boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
   boot.kernelModules = ["kvm-amd"];
   hardware.cpu.amd.updateMicrocode = config.hardware.enableRedistributableFirmware;
+
+  services = {
+    libinput = {
+      enable = true;
+      mouse.accelProfile = "flat";
+    };
+
+    xserver.xkb = {
+      layout = "us,kr";
+      options = "caps:ctrl_modifier";
+    };
+  };
 }
