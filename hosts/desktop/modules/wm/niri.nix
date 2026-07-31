084dd830561dcaa9cf078d4342ed0785215d6cbe
niodtn <ipegte93@gmail.com>
2026-08-01 02:11:17 +0900

    feat(desktop): update niri

--- hosts/desktop/modules/wm/niri.nix
+++ hosts/desktop/modules/wm/niri.nix
@@ -22,41 +22,56 @@
       sound = true;
     };
 
-    flake.aspects.host-desktop.nixos = {config, ...}:
-      lib.mkMerge [
-        # Wayland
-        {
-          environment = {
-            XDG_SESSION_TYPE = "wayland";
-            pathsToLink = ["/share/wayland-sessions"];
-          };
-        }
+    flake.aspects.host-desktop.nixos = lib.mkMerge [
+      # Wayland
+      ({pkgs, ...}: {
+        environment = {
+          variables.XDG_SESSION_TYPE = "wayland";
+          pathsToLink = ["/share/wayland-sessions"];
+          systemPackages = with pkgs; [xwayland-satellite];
+        };
+      })
 
-        # Greetd
-        {
-          services.greetd = {
+      # Greetd
+      ({config, ...}: {
+        services.greetd = {
+          enable = true;
+          settings.default_session = {
+            command = "dbus-run-session ${config.programs.niri.package}/bin/niri";
+            user = "niodtn";
+          };
+        };
+
+        systemd.user.services.niri.enableDefaultPath = false;
+      })
+
+      # Niri
+      ({config, ...}: {
+        home-manager.users.${config.host.userName} = {
+          wayland.windowManager.niri = {
             enable = true;
-            settings.default_session = {
-              command = "dbus-run-session ${config.programs.niri.package}/bin/niri";
-              user = "niodtn";
+
+            settings = {
+              prefer-no-csd = {};
+              hotkey-overlay.skip-at-startup = {};
+
+              _children = [
+                {spawn-at-startup._args = ["systemctl" "--user" "import-environment"];}
+                {spawn-at-startup._args = ["xwayland-satellite"];}
+                {spawn-at-startup._args = ["fcitx5" "-d"];}
+
+                # Suppress maximize events for all windows
+                {
+                  window-rule._children = [
+                    {open-maximized = false;}
+                    {open-fullscreen = false;}
+                  ];
+                }
+              ];
             };
           };
-
-          systemd.user.services.niri.enableDefaultPath = false;
-        }
-
-        # Niri
-        {
-          home-manager.users.${config.host.userName} = {
-            wayland.windowManager.niri = {
-              enable = true;
-
-              settings = {
-                prefer-no-csd = {};
-              };
-            };
-          };
-        }
-      ];
+        };
+      })
+    ];
   };
 }
