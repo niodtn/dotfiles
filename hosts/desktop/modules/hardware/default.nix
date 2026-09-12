d06aff78c4ceb05680e00a42532c38c418b1b323
niodtn <ipegte93@gmail.com>
2026-09-13 00:51:39 +0900

    feat(desktop): update hardware

--- hosts/desktop/modules/hardware/default.nix
+++ hosts/desktop/modules/hardware/default.nix
@@ -1,7 +1,18 @@
-{
-  flake.aspects.core.nixos = {
-    boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
-    hardware.enableAllFirmware = true;
-    zramSwap.enable = true;
-  };
+{lib, ...}: {
+  flake.aspects.core.nixos = lib.mkMerge [
+    {
+      boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
+      hardware.enableAllFirmware = true;
+    }
+
+    {
+      zramSwap.enable = true;
+      powerManagement.cpuFreqGovernor = "performance";
+
+      services = {
+        power-profiles-daemon.enable = false;
+        tlp.enable = false;
+      };
+    }
+  ];
 }
