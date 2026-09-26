58c10abdb65add793b2a2951b41b784161058549
niodtn <71699228+niodtn@users.noreply.github.com>
2026-09-26 18:07:11 +0900

    outputs.nix 업데이트

--- hosts/wsl/outputs.nix
+++ hosts/wsl/outputs.nix
@@ -18,7 +18,14 @@ inputs.flake-parts.lib.mkFlake {inherit inputs;} {
   flake.aspects.core.nixos = {config, ...}: {
     host.hostName = baseNameOf ./.;
 
+    # stateVersions
     system.stateVersion = "26.11";
     home-manager.users.${config.host.userName}.home.stateVersion = "26.11";
+
+    # Passwords
+    users.users = {
+      root.hashedPassword = "!";
+      ${config.host.userName}.hashedPassword = "$y$j9T$FiIoFpdVFv30Viq0WYsDS1$5VGzz7Itx1PEVGmnwOJJIN12YAfFQ3JoaaE6dBiyYd9";
+    };
   };
 }
